80f030e9c7367ae417d4e7b50258f0e4a83c83ab
dturnbu <60360077+dturnbu@users.noreply.github.com>
2020-09-21 20:09:55 -0500

    added the HAProxy deployment

--- argocd-deployments.tf
+++ argocd-deployments.tf
@@ -1,89 +1,119 @@
-
-# ---
-# apiVersion: apps/v1
-# kind: Deployment
-# metadata:
-#   labels:
-#     app.kubernetes.io/component: redis
-#     app.kubernetes.io/name: argocd-redis-ha-haproxy
-#     app.kubernetes.io/part-of: argocd
-#   name: argocd-redis-ha-haproxy
-# spec:
-#   replicas: 3
-#   revisionHistoryLimit: 1
-#   selector:
-#     matchLabels:
-#       app.kubernetes.io/name: argocd-redis-ha-haproxy
-#   strategy:
-#     type: RollingUpdate
-#   template:
-#     metadata:
-#       annotations:
-#         checksum/config: 790be9eae7c7e468c497c0256949ab96cb3f14b935c6702424647c3c60fba91c
-#       labels:
-#         app.kubernetes.io/name: argocd-redis-ha-haproxy
-#       name: argocd-redis-ha-haproxy
-#     spec:
-#       affinity:
-#         podAntiAffinity:
-#           preferredDuringSchedulingIgnoredDuringExecution:
-#           - podAffinityTerm:
-#               labelSelector:
-#                 matchLabels:
-#                   app.kubernetes.io/name: argocd-redis-ha-haproxy
-#               topologyKey: failure-domain.beta.kubernetes.io/zone
-#             weight: 100
-#           requiredDuringSchedulingIgnoredDuringExecution:
-#           - labelSelector:
-#               matchLabels:
-#                 app.kubernetes.io/name: argocd-redis-ha-haproxy
-#             topologyKey: kubernetes.io/hostname
-#       containers:
-#       - image: haproxy:2.0.4
-#         imagePullPolicy: IfNotPresent
-#         livenessProbe:
-#           httpGet:
-#             path: /healthz
-#             port: 8888
-#           initialDelaySeconds: 5
-#           periodSeconds: 3
-#         name: haproxy
-#         ports:
-#         - containerPort: 6379
-#           name: redis
-#         resources: {}
-#         volumeMounts:
-#         - mountPath: /usr/local/etc/haproxy
-#           name: data
-#         - mountPath: /run/haproxy
-#           name: shared-socket
-#       initContainers:
-#       - args:
-#         - /readonly/haproxy_init.sh
-#         command:
-#         - sh
-#         image: haproxy:2.0.4
-#         imagePullPolicy: IfNotPresent
-#         name: config-init
-#         resources: {}
-#         volumeMounts:
-#         - mountPath: /readonly
-#           name: config-volume
-#           readOnly: true
-#         - mountPath: /data
-#           name: data
-#       nodeSelector: {}
-#       securityContext:
-#         fsGroup: 1000
-#         runAsNonRoot: true
-#         runAsUser: 1000
-#       serviceAccountName: argocd-redis-ha-haproxy
-#       tolerations: null
-#       volumes:
-#       - configMap:
-#           name: argocd-redis-ha-configmap
-#         name: config-volume
-#       - emptyDir: {}
-#         name: shared-socket
-#       - emptyDir: {}
-#         name: data
+resource "kubernetes_deployment" "argocd_redis_ha_haproxy" {
+  metadata {
+    name = "argocd-redis-ha-haproxy"
+    labels = {
+      "app.kubernetes.io/component": "redis"
+      "app.kubernetes.io/name": "argocd-redis-ha-haproxy"
+      "app.kubernetes.io/part-of": "argocd"
+    }
+  }
+  spec {
+    replicas = 3
+    revision_history_limit = 1
+    selector {
+      match_labels = {
+        "app.kubernetes.io/name": "argocd-redis-ha-haproxy"
+      }
+    }
+    strategy {
+      type = "RollingUpdate"
+    }
+    template {
+      metadata {
+        name = "argocd-redis-ha-haproxy"
+        labels = {
+          "app.kubernetes.io/name": "argocd-redis-ha-haproxy"
+        }
+        annotations = {
+          "checksum/config": "790be9eae7c7e468c497c0256949ab96cb3f14b935c6702424647c3c60fba91c"
+        }
+      }
+      spec {
+        affinity {
+          pod_anti_affinity {
+            preferred_during_scheduling_ignored_during_execution {
+              weight = 100
+              pod_affinity_term {
+                topology_key = "failure-domain.beta.kubernetes.io/zone"
+                label_selector {
+                  match_labels = {
+                    "app.kubernetes.io/name": "argocd-redis-ha-haproxy"
+                  }
+                }
+              }
+            }
+            required_during_scheduling_ignored_during_execution {
+              topology_key = "kubernetes.io/hostname"
+              label_selector {
+                match_labels = {
+                  "app.kubernetes.io/name": "argocd-redis-ha-haproxy"
+                }
+              }
+            }
+          }
+        }
+        container {
+          name = "haproxy"
+          image = "${var.image_repository}/haproxy:${var.haproxy_version}"
+          image_pull_policy = var.image_pull_policy
+          liveness_probe {
+            http_get {
+              path = "/healthz"
+              port = 8888
+            }
+            initial_delay_seconds = 5
+            period_seconds = 3
+          }
+          port {
+            name = "redis"
+            container_port = 6379
+          }
+          # TODO: Resource requirements will need to be declared
+          resources {}
+          volume_mount {
+            name = "shared-socket"
+            mount_path = "/usr/local/etc/haproxy"
+          }
+        }
+        init_container {
+          name = "config-init"
+          image = "${var.image_repository}/haproxy:${var.haproxy_version}"
+          image_pull_policy = var.image_pull_policy
+          command = ["sh"]
+          args = ["/readonly/haproxy_init.sh"]
+          # TODO: Resource requirements will need to be declared
+          resources {}
+          volume_mount {
+            name = "config-volume"
+            mount_path = "/readonly"
+            read_only = true
+          }
+          volume_mount {
+            name = "data"
+            mount_path = "/data"
+          }
+        }
+        security_context {
+          run_as_non_root = var.run_as_non_root
+          fs_group = var.fs_group
+          run_as_user = var.run_as_user
+        }
+        service_account_name = "argocd-redis-ha-haproxy"
+        volume {
+          name = "config-volume"
+          config_map {
+            name = "argocd-redis-ha-configmap"
+          }
+        }
+        volume {
+          name = "shared-socket"
+          empty_dir {}
+        }
+        volume {
+          name = "data"
+          empty_dir {}
+        }
+      }
+    }
+  }
+}
--- variables.tf
+++ variables.tf
@@ -6,21 +6,49 @@ variable "argocd_version" {
   description = "The version of ArgoCD to deploy."
   default     = "1.7.6"
 }
+
 variable "namespace" {
   type        = string
   description = "The namespace this ArgoCD deployment will reside in."
   default     = "argocd"
 }
-variable "replicas" {
-  type        = number
-  description = "The number of replicas."
+
+variable "image_repository" {
+  type = string
+  description = "The image repository to use when pulling images."
+  default = "registry.hub.docker.com/library"
 }
-variable "container_networking" {
-  type        = object({ type : string, port : number, container_port : number, whitelist_cidr : string, security_groups : list(string) })
-  description = "An object that describes the service."
+variable "image_pull_policy" {
+  type = string
+  description = "Determines when the image should be pulled prior to starting the container. `Always`: Always pull the image. | `IfNotPresent`: Only pull the image if it does not already exist on the node. | `Never`: Never pull the image"
+  default = "Always"
 }
 variable "labels" {
   type        = map(string)
   description = "Extra Kubernetes labels to include with the resources created by this module"
   default     = {}
+}
+
+
+
+variable "haproxy_version" {
+  type = string
+  description = "Defines which docker image tag to use for HAProxy"
+  default = "2.0.4"
+}
+
+variable "fs_group" {
+  type = number
+  description = ""
+  default = 1000
+}
+variable "run_as_non_root" {
+  type = bool
+  description = ""
+  default = true
+}
+variable "run_as_user" {
+  type = number
+  description = ""
+  default = 1000
 }
