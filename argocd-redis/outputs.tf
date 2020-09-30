72c7b4a5bf08ffb34e66cc141711921e81f04970
Dylan Turnbull <dylanturn@gmail.com>
2020-09-30 07:28:52 -0500

    forgot the outputs

--- argocd-redis/outputs.tf
+++ argocd-redis/outputs.tf
@@ -1,8 +1,8 @@
 
 output "redis_address" {
-  value = module.argocd_haproxy_ha.address
+  value = module.argocd_haproxy_ha.0.address
 }
 
 output "redis_port" {
-  value = module.argocd_haproxy_ha.port
+  value = module.argocd_haproxy_ha.0.port
 }
