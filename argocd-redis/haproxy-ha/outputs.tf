9198d60a9ccbbd61d974dacd6010f62f82fab7ee
Dylan Turnbull <dylanturn@gmail.com>
2020-09-30 07:24:34 -0500

    forgot the outputs

--- argocd-redis/haproxy-ha/outputs.tf
+++ argocd-redis/haproxy-ha/outputs.tf
@@ -2,5 +2,5 @@ output "address" {
   value = kubernetes_service.haproxy_service.metadata.0.name
 }
 output "port" {
-  value = kubernetes_service.haproxy_service.spec.port.0.port
+  value = kubernetes_service.haproxy_service.spec.0.port.0.port
 }
