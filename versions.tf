85b867047e5bcc4365eda98b9a92892403339da7
Dylan Turnbull <dylanturn@gmail.com>
2020-10-04 09:37:57 -0500

    un-debugging

--- versions.tf
+++ versions.tf
@@ -1,12 +1,12 @@
 terraform {
   required_version = ">= 0.13"
-#  required_providers {
-#    kubernetes = {
-#      source = "hashicorp/kubernetes"
-#    }
-#    k8s = {
-#      version = ">= 0.8.0"
-#      source  = "banzaicloud/k8s"
-#    }
-#  }
+  required_providers {
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+    k8s = {
+      version = ">= 0.8.0"
+      source  = "banzaicloud/k8s"
+    }
+  }
 }
