b3f21e353060c26d375649794def6b26e156ffc5
Dylan Turnbull <dylanturn@gmail.com>
2021-03-29 09:41:10 -0500

    make sure that all of ArgoCD has been deployed before we output the namespace. This is done to prevent race conditions

--- outputs-argocd.tf
+++ outputs-argocd.tf
@@ -1,3 +1,8 @@
 output "namespace" {
+  depends_on = [
+    module.argocd_server,
+    module.argocd_repo_server,
+    module.argocd_application_controller
+  ]
   value = kubernetes_namespace.argocd_namespace.metadata.0.name
 }
