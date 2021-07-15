421227ff07db8d57d2cdf2f9a06d6ce8e799b7ea
Dylan Turnbull <dylanturn@gmail.com>
2021-07-15 14:27:17 +0000

    added resource customization to fix progressing ingress

--- locals.tf
+++ locals.tf
@@ -16,4 +16,20 @@ locals {
 
   argocd_server_limits_cpu    = var.argocd_server_limits.cpu == null ? "100m" : var.argocd_server_limits.cpu
   argocd_server_limits_memory = var.argocd_server_limits.memory == null ? "128Mi" : var.argocd_server_limits.memory
+
+    resource.customizations: |
+    argoproj.io/Application:
+      health.lua: |
+        hs = {}
+        hs.status = "Progressing"
+        hs.message = ""
+        if obj.status ~= nil then
+          if obj.status.health ~= nil then
+            hs.status = obj.status.health.status
+            if obj.status.health.message ~= nil then
+              hs.message = obj.status.health.message
+            end
+          end
+        end
+        return hs
 }
